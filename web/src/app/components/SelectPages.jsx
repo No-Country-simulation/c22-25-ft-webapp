0b7bce70e50309728a4c47cf5702a27810087094
Eduardo <ejrb1234@gmail.com>
2024-11-28 14:41:48 -0400

    <SelectPage/> => Componente momentáneo para visualizar todas las páginas

---
+++ web/src/app/components/SelectPages.jsx
@@ -0,0 +1,31 @@
+'use client'
+//  Este componente es momentáneo; para observar las páginas disponibles, 
+import { Select, SelectItem } from "@nextui-org/react";
+import Link from "next/link";
+
+const paginas = [
+  { key: 'inicio', label: 'Inicio', href: '/' },
+  { key: 'login', label: 'Login', href: '/login' },
+  { key: 'register', label: 'Register', href: '/register' },
+  { key: 'dashboard', label: 'Dashboard', href: '/dashboard' },
+  { key: 'unauthorized', label: 'Unauthorized', href: '/unauthorized' },
+  { key: 'notfound', label: 'Not Found', href: '/notfound' },
+];
+
+export const SelectPages = () => {
+  return (
+    <Select
+      label="Páginas posibles"
+      className="w-[172px] absolute bottom-2 right-2 opacity-40"
+      color="danger"
+    >
+      {paginas.map(({ key, label, href }) => (
+        <SelectItem key={key} className="flex" textValue={label}>
+          <Link href={href} className="flex">
+            {label}
+          </Link>
+        </SelectItem>
+      ))}
+    </Select>
+  )
+};
