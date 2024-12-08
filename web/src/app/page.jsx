1e2d25ff7fc7dd7965ce041cf39c0dcc391e4992
Eduardo <ejrb1234@gmail.com>
2024-12-08 14:31:22 -0400

    MetricsSection

--- web/src/app/page.jsx
+++ web/src/app/page.jsx
@@ -1,11 +1,15 @@
 import { HeaderLanding } from '@/components/landing-page/HeaderLanding'
 import { HeroSection } from '@/components/landing-page/HeroSection'
+import { FeaturesSection } from '@/components/landing-page/FeaturesSection'
+import { MetricsSection } from '@/components/landing-page/MetricsSection'
 
 export default function HomePage() {
   return (
     <>
       <HeaderLanding />
       <HeroSection />
+      <FeaturesSection />
+      <MetricsSection />
     </>
   )
 }
