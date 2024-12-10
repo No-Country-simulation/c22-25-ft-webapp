97304a6b52533a8b8a494c26832656ca6689abca
mrredu <ejrb1234@gmail.com>
2024-12-10 01:03:55 -0400

    getAge @util

--- web/src/app/utils/utils.js
+++ web/src/app/utils/utils.js
@@ -1,1 +1,12 @@
 export const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1)
+
+export const getAge = birthday => {
+  const today = new Date()
+  const birthDate = new Date(birthday)
+  let age = today.getFullYear() - birthDate.getFullYear()
+  const m = today.getMonth() - birthDate.getMonth()
+  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+    age--
+  }
+  return age
+}
