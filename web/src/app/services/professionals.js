a0c863618326f94775c97886e2c65a41670e1f9d
mrredu <ejrb1234@gmail.com>
2024-12-10 01:04:09 -0400

    professionals @services

---
+++ web/src/app/services/professionals.js
@@ -0,0 +1,33 @@
+export const getAllProfessionals = async token => {
+  if (!token) {
+    console.error('No se proporcionó un token de autorización.')
+    return []
+  }
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/admin/users`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+
+    if (!res.ok) {
+      const errorText = await res.text() // Obtén el texto del error
+      console.error('Error response:', res.status, errorText)
+      throw new Error(`Failed to fetch professionals: ${res.status}`)
+    }
+
+    if (res.status === 204) return []
+
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.error('Error fetching professionals:', error)
+    return []
+  }
+}
