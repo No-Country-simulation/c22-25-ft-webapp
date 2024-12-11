12d826a8f261ad42ca2f038be32472d8443e2790
mrredu <ejrb1234@gmail.com>
2024-12-11 17:20:10 -0400

    InformaciónBásica del paciente default

--- web/src/app/components/molecules/BasicInformation.jsx
+++ web/src/app/components/molecules/BasicInformation.jsx
@@ -1,76 +1,194 @@
 'use client'
 import { Accordion, AccordionItem } from '@nextui-org/react'
 
-const defaultContent =
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+const historialMedico = [
+  {
+    id: 1,
+    title: 'Antecedentes Personales',
+    content: 'Alergia al polen',
+  },
+  {
+    id: 2,
+    title: 'Antecedentes Familiares',
+    content: 'Historial de diabetes en la familia',
+  },
+]
+
+const motivoConsulta = [
+  {
+    id: 1,
+    title: 'Fecha de consulta',
+    content: '10 de diciembre de 2024',
+  },
+  {
+    id: 2,
+    title: 'Motivo de la consulta',
+    content: 'Dolor de cabeza persistente',
+  },
+  {
+    id: 3,
+    title: 'Enfermedad Actual',
+    content: 'Migrañas frecuentes',
+  },
+]
+
+const evaluacionDiagnostico = [
+  {
+    id: 1,
+    title: 'Examen Físico',
+    content: 'Presión arterial normal, signos vitales estables',
+  },
+  {
+    id: 2,
+    title: 'Diagnóstico',
+    content: 'Migraña',
+  },
+]
+
+const planTratamiento = [
+  {
+    id: 1,
+    title: 'Tratamiento',
+    content: 'Prescripción de analgésicos y recomendación de descanso',
+  },
+  {
+    id: 2,
+    title: 'Procedimientos Médicos',
+    content: 'Ninguno necesario',
+  },
+  {
+    id: 3,
+    title: 'Evolución',
+    content: 'Seguir monitorizando la frecuencia e intensidad de las migrañas',
+  },
+]
+
+const documentacionConsentimientos = [
+  {
+    id: 1,
+    title: 'Consentimiento Informado',
+    content: 'Firmado',
+  },
+  {
+    id: 2,
+    title: 'Información Adicional',
+    content: 'Ninguna',
+  },
+]
+
 export const BasicInformation = () => {
   return (
-    <Accordion defaultExpandedKeys={['1']} selectionMode="multiple">
-      <AccordionItem
+    <Accordion defaultExpandedKeys={['2']} selectionMode="multiple">
+      {/* <AccordionItem
         key="1"
         aria-label="Información básica"
         title={
-          <h2 className="text-cloud-300 text-sm font-bold uppercase">
+          <h2 className="text-cloud-600 text-sm font-bold uppercase">
             Información básica
           </h2>
         }
       >
         {defaultContent}
-      </AccordionItem>
+      </AccordionItem> */}
       <AccordionItem
         key="2"
         aria-label="Historial médico"
         title={
-          <h2 className="text-cloud-300 text-sm font-bold uppercase">
+          <h2 className="text-cloud-600 text-sm font-bold uppercase">
             Historial médico
           </h2>
         }
       >
-        {defaultContent}
+        <ul>
+          {historialMedico.map(item => (
+            <li key={item.id} className="flex items-baseline gap-2">
+              <h3 className="text-cloud-300 text-sm font-bold uppercase">
+                {item.title}:
+              </h3>
+              <span>{item.content}</span>
+            </li>
+          ))}
+        </ul>
       </AccordionItem>
       <AccordionItem
         key="3"
         aria-label="Motivo de consulta"
         title={
-          <h2 className="text-cloud-300 text-sm font-bold uppercase">
+          <h2 className="text-cloud-600 text-sm font-bold uppercase">
             Motivo de consulta
           </h2>
         }
       >
-        {defaultContent}
+        <ul>
+          {motivoConsulta.map(item => (
+            <li key={item.id} className="flex items-baseline gap-2">
+              <h3 className="text-cloud-300 text-sm font-bold uppercase">
+                {item.title}:
+              </h3>
+              <span>{item.content}</span>
+            </li>
+          ))}
+        </ul>
       </AccordionItem>
       <AccordionItem
         key="4"
         aria-label="Evaluación y Diagnóstico"
         title={
-          <h2 className="text-cloud-300 text-sm font-bold uppercase">
+          <h2 className="text-cloud-600 text-sm font-bold uppercase">
             Evaluación y Diagnóstico
           </h2>
         }
       >
-        {defaultContent}
+        <ul>
+          {evaluacionDiagnostico.map(item => (
+            <li key={item.id} className="flex items-baseline gap-2">
+              <h3 className="text-cloud-300 text-sm font-bold uppercase">
+                {item.title}:
+              </h3>
+              <span>{item.content}</span>
+            </li>
+          ))}
+        </ul>
       </AccordionItem>
       <AccordionItem
         key="5"
         aria-label="Plan de Tratamiento y Seguimiento"
         title={
-          <h2 className="text-cloud-300 text-sm font-bold uppercase">
+          <h2 className="text-cloud-600 text-sm font-bold uppercase">
             Plan de Tratamiento y Seguimiento
           </h2>
         }
       >
-        {defaultContent}
+        <ul>
+          {planTratamiento.map(item => (
+            <li key={item.id} className="flex items-baseline gap-2">
+              <h3 className="text-cloud-300 text-sm font-bold uppercase">
+                {item.title}:
+              </h3>
+              <span>{item.content}</span>
+            </li>
+          ))}
+        </ul>
       </AccordionItem>
       <AccordionItem
         key="6"
         aria-label="Documentación y Consentimientos"
         title={
-          <h2 className="text-cloud-300 text-sm font-bold uppercase">
+          <h2 className="text-cloud-600 text-sm font-bold uppercase">
             Documentación y Consentimientos
           </h2>
         }
       >
-        {defaultContent}
+        <ul>
+          {documentacionConsentimientos.map(item => (
+            <li key={item.id} className="flex items-baseline gap-2">
+              <h3 className="text-cloud-300 text-sm font-bold uppercase">
+                {item.title}:
+              </h3>
+              <span>{item.content}</span>
+            </li>
+          ))}
+        </ul>
       </AccordionItem>
     </Accordion>
   )
