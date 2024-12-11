72afdb05859e19fcfa69927a529075512c2df973
mrredu <ejrb1234@gmail.com>
2024-12-11 18:44:14 -0400

    Eliminado modal para navegar

--- web/src/app/components/Providers.jsx
+++ web/src/app/components/Providers.jsx
@@ -1,15 +1,11 @@
 'use client'
 import propTypes from 'prop-types'
 import { NextUIProvider } from '@nextui-org/system'
-import { SelectPages } from './SelectPages'
 import { SessionProvider } from 'next-auth/react'
 export const Providers = ({ children, session }) => {
   return (
     <SessionProvider session={session}>
-      <NextUIProvider>
-        {children}
-        <SelectPages />
-      </NextUIProvider>
+      <NextUIProvider>{children}</NextUIProvider>
     </SessionProvider>
   )
 }
