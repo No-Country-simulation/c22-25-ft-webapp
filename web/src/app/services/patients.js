ae068d204d951f6efb8cef234ee439d75473d3d6
mrredu <ejrb1234@gmail.com>
2024-12-10 20:07:28 -0400

    services

--- web/src/app/services/patients.js
+++ web/src/app/services/patients.js
@@ -1,12 +1,12 @@
-export const getAllPatients = async token => {
+export const getPatientByDNI = async ({ token, doctorDni, patientDni }) => {
   if (!token) {
     console.error('No se proporcionó un token de autorización.')
-    return []
+    return null
   }
 
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/admin/patients`,
+      `${process.env.NEXT_PUBLIC_API_URL}/api/doctor/${doctorDni}/patients/${patientDni}`,
       {
         method: 'GET',
         headers: {
@@ -15,6 +15,44 @@ export const getAllPatients = async token => {
         },
       }
     )
+
+    if (!res.ok) {
+      const errorText = await res.text()
+      console.error('Error response:', res.status, errorText)
+      throw new Error(`Failed to fetch patient: ${res.status}`)
+    }
+
+    if (res.status === 204) return null
+
+    const data = await res.json()
+    return data
+  } catch (error) {
+    console.error('Error fetching patient:', error)
+    return null
+  }
+}
+
+export const getAllPatients = async ({ token, doctorDni, rol }) => {
+  if (!token) {
+    console.error('No se proporcionó un token de autorización.')
+    return []
+  }
+
+  let URL
+  if (rol === 'doctor') {
+    URL = `${process.env.NEXT_PUBLIC_API_URL}/api/doctor/${doctorDni}/patients`
+  } else {
+    URL = `${process.env.NEXT_PUBLIC_API_URL}/api/admin/patients`
+  }
+
+  try {
+    const res = await fetch(URL, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    })
 
     if (!res.ok) {
       const errorText = await res.text()
