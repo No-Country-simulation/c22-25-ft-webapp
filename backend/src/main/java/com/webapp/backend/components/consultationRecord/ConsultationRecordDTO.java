5990f597b5ec126363c8f66bb2b3852e40acff4e
mrredu <ejrb1234@gmail.com>
2024-12-12 18:57:49 -0400

    No son requeridos los parámetros de consulta

--- backend/src/main/java/com/webapp/backend/components/consultationRecord/ConsultationRecordDTO.java
+++ backend/src/main/java/com/webapp/backend/components/consultationRecord/ConsultationRecordDTO.java
@@ -1,6 +1,6 @@
 package com.webapp.backend.components.consultationRecord;
 
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,45 +10,45 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class ConsultationRecordDTO {
-    private Integer consultationRecordId;
-    @NotEmpty(message = "reason is mandatory")
-    private String reason;
-    @NotEmpty(message = "currentIllness is mandatory")
-    private String currentIllness;
-    @NotEmpty(message = "personalBackground is mandatory")
-    private String personalBackground;
-    @NotEmpty(message = "familiarBackground is mandatory")
-    private String familiarBackground;
-    @NotEmpty(message = "physical is mandatory")
-    private String physicalExam;
-    @NotEmpty(message = "dx is mandatory")
-    private String dx;
-    @NotEmpty(message = "treatment is mandatory")
-    private String treatment;
-    @NotEmpty(message = "evolution is mandatory")
-    private String evolution;
-    @NotEmpty(message = "diagnosticTest is mandatory")
-    private String diagnosticTests;
-    @NotEmpty(message = "medProcedures is mandatory")
-    private String medProcedures;
-    @NotEmpty(message = "informedConsent is mandatory")
-    private String informedConsent;
-    @NotEmpty(message = "aditionalInformation is mandatory")
-    private String aditionalInformation;
+  private Integer consultationRecordId;
+  @NotNull(message = "reason is mandatory")
+  private String reason;
+  @NotNull(message = "currentIllness is mandatory")
+  private String currentIllness;
+  @NotNull(message = "personalBackground is mandatory")
+  private String personalBackground;
+  @NotNull(message = "familiarBackground is mandatory")
+  private String familiarBackground;
+  @NotNull(message = "physical is mandatory")
+  private String physicalExam;
+  @NotNull(message = "dx is mandatory")
+  private String dx;
+  @NotNull(message = "treatment is mandatory")
+  private String treatment;
+  @NotNull(message = "evolution is mandatory")
+  private String evolution;
+  @NotNull(message = "diagnosticTest is mandatory")
+  private String diagnosticTests;
+  @NotNull(message = "medProcedures is mandatory")
+  private String medProcedures;
+  @NotNull(message = "informedConsent is mandatory")
+  private String informedConsent;
+  @NotNull(message = "aditionalInformation is mandatory")
+  private String aditionalInformation;
 
-    public ConsultationRecordDTO(ConsultationRecord consultationRecord) {
-        this.consultationRecordId = consultationRecord.getConsultationRecordId();
-        this.reason = consultationRecord.getReason();
-        this.currentIllness = consultationRecord.getCurrentIllness();
-        this.personalBackground = consultationRecord.getPersonalBackground();
-        this.familiarBackground = consultationRecord.getFamiliarBackground();
-        this.physicalExam = consultationRecord.getPhysicalExam();
-        this.dx = consultationRecord.getDx();
-        this.treatment = consultationRecord.getTreatment();
-        this.evolution = consultationRecord.getEvolution();
-        this.diagnosticTests = consultationRecord.getDiagnosticTests();
-        this.medProcedures = consultationRecord.getMedProcedures();
-        this.informedConsent = consultationRecord.getInformedConsent();
-        this.aditionalInformation = consultationRecord.getAditionalInformation();
-    }
+  public ConsultationRecordDTO(ConsultationRecord consultationRecord) {
+    this.consultationRecordId = consultationRecord.getConsultationRecordId();
+    this.reason = consultationRecord.getReason();
+    this.currentIllness = consultationRecord.getCurrentIllness();
+    this.personalBackground = consultationRecord.getPersonalBackground();
+    this.familiarBackground = consultationRecord.getFamiliarBackground();
+    this.physicalExam = consultationRecord.getPhysicalExam();
+    this.dx = consultationRecord.getDx();
+    this.treatment = consultationRecord.getTreatment();
+    this.evolution = consultationRecord.getEvolution();
+    this.diagnosticTests = consultationRecord.getDiagnosticTests();
+    this.medProcedures = consultationRecord.getMedProcedures();
+    this.informedConsent = consultationRecord.getInformedConsent();
+    this.aditionalInformation = consultationRecord.getAditionalInformation();
+  }
 }
